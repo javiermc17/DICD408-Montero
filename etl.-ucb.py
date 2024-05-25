806edeed0c99b1953f1277fd13c3d93fce86df0f
javiermc17 <javier.montero@ucb.edu.bo>
2024-05-25 00:09:02 +0000

    Extraccion

---
+++ etl.-ucb.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+# Extraccion
+wine_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
+wine_data = pd.read_csv(wine_url, header=None)
+
+wine_quality_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+wine_quality_data = pd.read_csv(wine_quality_url, sep=";")
+
+#Initial look at the data
+print (wine_data.head())
+print(wine_quality_data.head())
+
+
+
+
+
